7367e5c4826a19fc3df82a7f47d39c87ad761a81
Banelengubeni <155159669+Banelengubeni@users.noreply.github.com>
2024-02-12 13:00:27 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
-  return <div className="App">banele</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Johannesburg" />
+        banele
+      </div>
+    </div>
+  );
 }
 
 export default App;
